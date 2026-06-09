59dcbe703451f2cf4dcceacf5c3aea7128f62d5b
Subash <bhattasubash970@gmail.com>
2026-06-09 13:28:03 +0530

    Increase logo height by 10px and width proportionally

--- dashboard/src/app/dashboard/layout.tsx
+++ dashboard/src/app/dashboard/layout.tsx
@@ -28,7 +28,7 @@ export default async function DashboardLayout({
         <div>
           {/* Logo */}
           <div className="p-6 border-b border-border/50 flex items-center">
-            <img src="/huntly_logo.png" alt="Huntly Logo" className="h-[48px] w-[72px]" />
+            <img src="/huntly_logo.png" alt="Huntly Logo" className="h-[58px] w-[87px]" />
           </div>
 
           {/* Navigation Links */}
