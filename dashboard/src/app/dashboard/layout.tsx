d04f971607b39d71cd4b91822de8ae4966d0a96c
Subash <bhattasubash970@gmail.com>
2026-06-09 12:56:34 +0530

    Increase Huntly logo height to h-14 for standard elements and h-10 for mockup

--- dashboard/src/app/dashboard/layout.tsx
+++ dashboard/src/app/dashboard/layout.tsx
@@ -28,7 +28,7 @@ export default async function DashboardLayout({
         <div>
           {/* Logo */}
           <div className="p-6 border-b border-border/50 flex items-center">
-            <img src="/huntly_logo.png" alt="Huntly Logo" className="h-11 w-auto object-contain" />
+            <img src="/huntly_logo.png" alt="Huntly Logo" className="h-14 w-auto object-contain" />
           </div>
 
           {/* Navigation Links */}
